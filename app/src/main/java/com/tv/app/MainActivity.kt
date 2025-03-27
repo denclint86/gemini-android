654e3a29bd977f2f63647e458c16e9fb237d6fc7
denclint86 <rendezvous.jv@gmail.com>
2025-03-27 15:12:35 +0800

    feat: implement a automatic func-call test

--- app/src/main/java/com/tv/app/MainActivity.kt
+++ app/src/main/java/com/tv/app/MainActivity.kt
@@ -2,9 +2,9 @@ package com.tv.app
 
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.ViewModelProvider
-import com.tv.app.databinding.ActivityMainBinding
 import com.tv.app.chat.ChatViewModel
 import com.tv.app.chat.GenerativeViewModelFactory
+import com.tv.app.databinding.ActivityMainBinding
 import com.zephyr.vbclass.ViewBindingActivity
 
 class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
@@ -18,22 +18,6 @@ class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
             GenerativeViewModelFactory
         )[ChatViewModel::class.java]
 
-        viewModel.sendMessage("介绍习近平")
-//        流式示例
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            try {
-//                val responseFlow =
-//                    generativeModel.generateContentStream(prompt = "this is an api test, intro yourself")
-//                var currentText = ""
-//                responseFlow.collect { chunk ->
-//                    chunk.text?.let { text ->
-//                        currentText += text
-//                        println(text)
-//                    }
-//                }
-//            } catch (t: Throwable) {
-//                println(t.toLogString())
-//            }
-//        }
+        viewModel.sendMessage("我在测试api,你现在应该有一个可用api,说说这是个什么样的api，然后分别使用'niki'和'asd'去调用他,最后告诉我你得到的结果")
     }
 }
